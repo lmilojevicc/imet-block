dd61d8f9c79140432848f0aa2ef26170c7450519
Luka <milojevic.luka2002@gmail.com>
2024-11-16 17:39:52 +0100

    Updated script

--- index.user.js
+++ index.user.js
@@ -1,39 +1,64 @@
 // ==UserScript==
-// @name        Blokator Nebojse i Meti Retarda
+// @name        Blokator mentora i Meti Retarda
 // @run-at      document-idle
 // @author      Top Dzi
 // @namespace   Violentmonkey Scripts
 // @match       https://imet.metropolitan.ac.rs/student/#/home
 // @grant       none
-// @version     1.0
-// @description Digne ti se pritisak kada vidis Nebojsu i Meti Bota na IMET-u? Top Dzi je dosao da ti pomogne!
+// @version     1.1
+// @description Digne ti se pritisak kada vidis mentora i Meti Bota na IMET-u? Top Dzi je dosao da ti pomogne!
 // ==/UserScript==
 
-let runCount = 0;
-const intervalId = setInterval(() => {
-  const botRetard = document.querySelector('#metiDialog');
-  if (botRetard) {
-    botRetard.remove();
+function handleUrlChange() {
+  if (
+    window.location.href === "https://imet.metropolitan.ac.rs/student/#/home"
+  ) {
+    runScript();
   }
+}
 
-  const mentor = document
-    .querySelector('.mentor-box p.full-name')
-    .textContent.toLowerCase();
-
-  if (mentor.includes('nebojša gavrilović')) {
-    const myNode = document.querySelector('.content.mentor');
-    while (myNode.firstChild) {
-      myNode.removeChild(myNode.lastChild);
+function runScript() {
+  const intervalId = setInterval(() => {
+    const botRetard = document.querySelector("#metiDialog");
+    if (botRetard) {
+      botRetard.remove();
     }
-    const message = document.createElement('h2');
-    message.textContent = 'Bolje da ne znas ko ti je mentor';
-    myNode.appendChild(message);
-    const wrapper = document.querySelector('.mentor-box');
-    wrapper.style.minHeight = 'auto';
-  }
 
-  runCount++;
-  if (runCount >= 5) {
-    clearInterval(intervalId);
+    const mentorElement = document.querySelector(".mentor-box p.full-name");
+    if (mentorElement) {
+      const mentor = mentorElement.textContent.toLowerCase();
+      if (mentor.includes("nebojša gavrilović")) {
+        const myNode = document.querySelector(".content.mentor");
+        while (myNode.firstChild) {
+          myNode.removeChild(myNode.lastChild);
+        }
+        const message = document.createElement("h2");
+        message.textContent = "Bolje da ne znas ko ti je mentor";
+        myNode.appendChild(message);
+        const wrapper = document.querySelector(".mentor-box");
+        wrapper.style.minHeight = "auto";
+      }
+    }
+  }, 75);
+
+  return () => clearInterval(intervalId);
+}
+
+let cleanup = null;
+
+const observer = new MutationObserver(() => {
+  if (cleanup) {
+    cleanup();
   }
-}, 75);
+  cleanup = runScript();
+});
+
+observer.observe(document.querySelector("body"), {
+  childList: true,
+  subtree: true,
+});
+
+cleanup = runScript();
+
+window.addEventListener("popstate", handleUrlChange);
+window.addEventListener("hashchange", handleUrlChange);
